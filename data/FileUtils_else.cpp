9e787675f5c5e5be7b9c6ed53a2ab883d51b94f1
Stals <superstals@gmail.com>
2014-04-27 14:25:12 +0400

    fix for win

--- data/FileUtils_else.cpp
+++ data/FileUtils_else.cpp
@@ -6,7 +6,7 @@
 
 std::string FileUtils::getFullPath(const std::string& filename)
 {
-	string path("");
+	std::string path("");
     
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
 	// In android, every programe has a director under /data/data.
@@ -17,7 +17,7 @@ std::string FileUtils::getFullPath(const std::string& filename)
     
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
 	// You can save file in anywhere if you have the permision.
-	path.append("C:/");
+	;//path.append("C:/");
 #endif
     
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WOPHONE)
