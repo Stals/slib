0ae9b30ec4f42240c2d0543de809e767f284faf0
Stals <superstals@gmail.com>
2014-05-10 13:40:16 +0400

    progressbar from left to right

--- cocos/gui/ProgressBar.cpp
+++ cocos/gui/ProgressBar.cpp
@@ -9,17 +9,18 @@ ProgressBar::ProgressBar(CCSprite* emptyBar, CCSprite* fullBar)
 	
 	this->addChild(emptyBar);
 	//this->addChild(fullBar);
-	emptyBar->setAnchorPoint(ccp(0.5,0.5));
-	fullBar->setAnchorPoint(ccp(0.5,0.5));
+	emptyBar->setAnchorPoint(ccp(0.5, 0.5));
+	fullBar->setAnchorPoint(ccp(0.5, 0.5));
 
 
 	autorelease();
 	
 	fuelBar = CCProgressTimer::create(fullBar);
 	fuelBar->setType(cocos2d::kCCProgressTimerTypeBar);
-	fuelBar->setAnchorPoint(ccp(0.5,0.5));
+	fuelBar->setAnchorPoint(ccp(0.5, 0.5));
 	fuelBar->setBarChangeRate(ccp(1,0));
-	fuelBar->setTag(1);                 
+	fuelBar->setTag(1); 
+	fuelBar->setMidpoint(ccp(0, 0));
 
 	this->addChild(fuelBar);
 	fuelBar->setPercentage(100); // Value between 0-100
